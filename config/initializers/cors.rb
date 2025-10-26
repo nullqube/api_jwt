ae06ab2e240eadbd02f507d3a1bfe2f0e70fd702
nullqube <nullqube@yahoo.com>
2025-10-26 22:29:16 +0330

    feat(auth): update cors config

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,8 +2,9 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "*"  # Adjust for Ionic
     resource "*",
-             headers: :any,
-             methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
-             credentials: false
+        headers: :any,
+        expose: [ "Authorization", "X-Refresh-Token", "X-Device-Id", "X-Device-Name" ],
+        methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+        credentials: false
   end
 end
