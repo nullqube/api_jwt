ab155b5ddce25f02808e93c18a0ea7e50803ad67
nullqube <nullqube@yahoo.com>
2025-10-26 22:30:24 +0330

    feat(auth): routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,23 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      # Auth routes
+      post "auth/signup", to: "auth#signup"
+      post "auth/login", to: "auth#login"
+      post "auth/refresh", to: "auth#refresh"
+      delete "auth/logout", to: "auth#logout"
+      delete "auth/logout_all", to: "auth#logout_all"
+      get "auth/me", to: "auth#me"
+      get "auth/sessions", to: "auth#sessions"
+
+      # Add your other API routes here
+      # resources :posts
+      # resources :comments
+    end
+  end
+  get "test/hello", to: "test#hello"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
 end
