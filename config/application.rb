ba3003d324bf8450d92707a7e9725144ad430236
nullqube <nullqube@yahoo.com>
2025-10-26 22:36:11 +0330

    feat(auth): add rack attack middleware to app config

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,8 @@ module ApiJwt
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # Add Rack::Attack middleware
+    config.middleware.use Rack::Attack
   end
 end
